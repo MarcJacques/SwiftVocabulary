9673c466cd5185ac61e14755d2beb9a6812a6512
Marc <marc.daniel.jacques@gmail.com>
2020-10-24 01:34:24 -0400

    MVP. updated constraints and some spelling

--- Swift Vocabulary/Swift Vocabulary/View Controllers/DefinitionViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/View Controllers/DefinitionViewController.swift
@@ -10,23 +10,15 @@ import UIKit
 class DefinitionViewController: UIViewController {
    
     @IBOutlet weak var vocabularyWordLabel: UILabel!
-    
     @IBOutlet weak var definitionTextView: UITextView!
+    
+    var word: VocabularyWord?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        vocabularyWordLabel.text = word?.word
+        definitionTextView.text = word?.definition
 
-        // Do any additional setup after loading the view.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- Swift Vocabulary/Swift Vocabulary/View Controllers/WordsTableViewController.swift
+++ Swift Vocabulary/Swift Vocabulary/View Controllers/WordsTableViewController.swift
@@ -11,48 +11,58 @@ class WordsTableViewController: UITableViewController {
     
     var vocabularyWords = [VocabularyWord]()
     
-    let variable = VocabularyWord(word: "Variable", definition: "Is a mutable property used to store various data types. The value or data can be updated. Ususally indicated by 'var.'")
+    let variable = VocabularyWord(word: "Variable", definition: "Is a mutable property used to store various data types. The value or data can be updated. In Swift, variables are is indicated by 'var.'")
     let constant = VocabularyWord(word: "Constant", definition: "An immutable property used to store various data types. The value or data stored can not be changed. In Swift, a constant is indicated by 'let'.")
-    let function = VocabularyWord(word: "Function", definition: "Is code that can be reused. This is usually a set of instructions or commands to get an expected output. In swift functions are indicated by 'func'.")
-
+    let function = VocabularyWord(word: "Function", definition: "Is code that can be reused. This is usually a set of instructions or commands to get an expected output. In Swift, functions are indicated by 'func'.")
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         vocabularyWords = [variable, constant, function]
-
-    
+        
+        
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         return vocabularyWords.count
     }
-
+    
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "WordCell", for: indexPath)
-
-        // Configure the cell...
-
+        
+        let word = vocabularyWords[indexPath.row]
+        cell.textLabel?.text = word.word
+        
         return cell
     }
     
-
+    
     
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
+        if segue.identifier == "ShowDefinitionSegue" {
+            
+            guard let definitionVC = segue.destination as? DefinitionViewController,
+                  let indexPath = tableView.indexPathForSelectedRow else {
+                return
+            }
+            let vocabWord = vocabularyWords[indexPath.row]
+            definitionVC.word = vocabWord
+        }
         // Pass the selected object to the new view controller.
     }
-
-
+    
+    
 }
